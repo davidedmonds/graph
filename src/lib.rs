e5857e8c5a42110aa00d41e4a1f30674a0311765
David Edmonds <edmonds.d.r@gmail.com>
2014-10-22 16:36:02 +0100

    Convert to library.

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#![feature(globs)]
+#![feature(unsafe_destructor)]
+
+extern crate libc;
+extern crate gl;
+extern crate glutin;
+extern crate native;
+
+use gl::types::*;
+
+mod screen;
+mod program;
